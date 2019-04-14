a930be4d91c7686ae5eaea77d5627f54ec3ee3df
rballa201 <47977057+rballa201@users.noreply.github.com>
2019-04-14 13:22:19 +0100

    Update words.js

--- js/words.js
+++ js/words.js
@@ -5,7 +5,17 @@
 
 //When the page is fully loaded the page the script will kick in
 $(document).ready(function () {
-
+    function isIE() {
+        ua = navigator.userAgent;
+        /* MSIE used to detect old browsers and Trident used to newer ones*/
+        var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+        
+        return is_ie; 
+      }
+      /* Create an alert to show if the browser is IE or not */
+      if (isIE()){
+          alert('This Page does not support Internet Explorer');
+      }
     //Ajax call for the API data
     function getTargets(apiFn, sourceWord, max, response) {
         $.ajax({
